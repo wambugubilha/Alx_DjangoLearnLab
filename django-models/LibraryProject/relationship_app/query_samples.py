b125b7db2061f798aa0e6ad5f4786d0face4a1e1
wambugubilha <bilhahhwamb@gmail.com>
2025-09-06 23:43:54 +0300

    commit the changes made to the retrieving library query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -14,4 +14,4 @@ def books_in_library(library_name):
 # 3️⃣ Retrieve the librarian for a library
 def librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library=library)
