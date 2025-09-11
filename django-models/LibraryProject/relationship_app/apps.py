f0743e1df3248490ba4ba36644ab2ca4a3d98350
wambugubilha <bilhahhwamb@gmail.com>
2025-09-11 23:11:08 +0300

    commit the new changes made to the apps and signals files

--- django-models/LibraryProject/relationship_app/apps.py
+++ django-models/LibraryProject/relationship_app/apps.py
@@ -1,13 +1,7 @@
 from django.apps import AppConfig
 
 class RelationshipAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'relationship_app'
 
     def ready(self):
-        try:
-            from ..relationship_app import signals
-            signals = signals
-        except ImportError as e:
-            import logging
-            logging.error(f"Failed to import signals: {e}")
+        from  ..relationship_app.signals  # ✅ Absolute import
--- django-models/LibraryProject/relationship_app/signals.py
+++ django-models/LibraryProject/relationship_app/signals.py
@@ -1,7 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import UserProfile
+from ..relationship_app.models import UserProfile  # ✅ Absolute import
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
