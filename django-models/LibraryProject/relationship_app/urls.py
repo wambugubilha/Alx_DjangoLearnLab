97592603c3085fdf414bfc0d5ed06014dbb90f93
wambugubilha <bilhahhwamb@gmail.com>
2025-09-11 23:24:29 +0300

    commit the changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,9 +5,9 @@ from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
-    path('admin-view/', views.admin_view, name='admin_view'),
-    path('librarian-view/', views.librarian_view, name='librarian_view'),
-    path('member-view/', views.member_view, name='member_view'),
+    path('Admin-view/', views.admin_view, name='admin_view'),
+    path('Librarian-view/', views.librarian_view, name='librarian_view'),
+    path('Member-view/', views.member_view, name='member_view'),
     path('', include('relationship_app.urls')),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
