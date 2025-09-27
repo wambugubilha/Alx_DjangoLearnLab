24a73521c20e9a03c2616644407cf5a293e0e806
wambugubilha <bilhahhwamb@gmail.com>
2025-09-27 12:15:05 +0300

    commit the filtering , searching and ordering  updates made to the views file

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -40,6 +40,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ]
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
