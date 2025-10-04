5dbeb2da7af1dcff083086c197fe7040e610e037
wambugubilha <bilhahhwamb@gmail.com>
2025-10-04 14:06:16 +0300

    commit the updated tag files

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Tag
 from .models import Comment
+from taggit.forms import TagWidget
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -19,6 +20,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content", "tags"]
+        widgets = {
+            "tags": TagWidget(),
+        }
+
+
 
     def save(self, commit=True):
         instance = super().save(commit=False)
