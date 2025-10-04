1acd3fee05d970c3a3621ea1da1bf86348375fe8
wambugubilha <bilhahhwamb@gmail.com>
2025-10-04 13:07:33 +0300

    commit the updated urls file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
     path("post/<int:post_id>/comments/new/", add_comment, name="comment-add"),
-    path("post/<int:post_id>/comments/new/", CommentCreateView.as_view(), name="comment-add"),
+    path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="comment-add"),
     path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
     path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
 ]
