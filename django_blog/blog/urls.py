55f65de5e0a14e6c532298f3954503024cd88660
wambugubilha <bilhahhwamb@gmail.com>
2025-10-04 11:51:56 +0300

    commit the changes made to the urls file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,10 +11,10 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
     path("register/", views.register_view, name="register"),
     path("profile/", views.profile_view, name="profile"),
-    path("posts/", PostListView.as_view(), name="post-list"),
-    path("posts/new/", PostCreateView.as_view(), name="post-create"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("posts/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
-    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("post/", PostListView.as_view(), name="post-list"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 
 ]
