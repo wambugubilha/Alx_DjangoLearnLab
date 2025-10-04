8545cad0c3a7e413a908b44a4239d5be3866f10f
wambugubilha <bilhahhwamb@gmail.com>
2025-10-04 13:02:42 +0300

    commit the comment management files

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,8 +18,8 @@ urlpatterns = [
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
     path("post/<int:post_id>/comments/new/", add_comment, name="comment-add"),
-    path("posts/<int:post_id>/comments/new/", CommentCreateView.as_view(), name="comment-add"),
-    path("comments/<int:pk>/edit/", CommentUpdateView.as_view(), name="comment-update"),
-    path("comments/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
+    path("post/<int:post_id>/comments/new/", CommentCreateView.as_view(), name="comment-add"),
+    path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
+    path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
 ]
 
