3e89b0e87b8c97de1dd2559c4543cac824499d47
wambugubilha <bilhahhwamb@gmail.com>
2025-10-12 15:29:29 +0300

    commit the views updated file

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -53,7 +53,7 @@ class LikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
         if not created:
             return Response({'detail': 'Already liked.'}, status=status.HTTP_400_BAD_REQUEST)
